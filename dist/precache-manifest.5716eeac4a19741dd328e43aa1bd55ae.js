39fc9de449f39b15462a204e7866933e5c1ff780
chaos <chaos@san.com>
2019-06-22 21:10:33 +0800

    feat: test

---
+++ dist/precache-manifest.5716eeac4a19741dd328e43aa1bd55ae.js
@@ -0,0 +1,36 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e1383378704061268d216c70b9b6f59",
+    "url": "index.html"
+  },
+  {
+    "url": "main.1.841bd73a1e6707044bac.bundle.js"
+  },
+  {
+    "url": "presenter.view.2.d95ff37726773e78c1ba.bundle.js"
+  },
+  {
+    "url": "runtime.676586e4444f80732d68.bundle.js"
+  },
+  {
+    "url": "target-blank.4.855b42986a0fa6552969.bundle.js"
+  },
+  {
+    "url": "vendor.5.b0a2a33bba500015fb09.bundle.js"
+  },
+  {
+    "url": "vendor.5.b0a2a33bba500015fb09.css"
+  },
+  {
+    "url": "vendors~Sidebar.0.0d25f21eb92a85bf27fc.bundle.js"
+  },
+  {
+    "url": "vendors~Sidebar.0.0d25f21eb92a85bf27fc.css"
+  },
+  {
+    "url": "vendors~live.6.12724f121c6c0aa50c26.bundle.js"
+  },
+  {
+    "url": "vendors~live.6.12724f121c6c0aa50c26.css"
+  }
+]);
