d164c55beb1650d2351df4ed3168ed33c8b91e36
chaos <chaos@san.com>
2019-06-22 22:12:32 +0800

    Updates

---
+++ precache-manifest.1174060eec21eca77ada1c5a3005f182.js
@@ -0,0 +1,36 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cee3cb050f55a904082a6557fee3e32",
+    "url": "index.html"
+  },
+  {
+    "url": "main.1.fea802ce2b8d793aaf9b.bundle.js"
+  },
+  {
+    "url": "presenter.view.2.7f2c73d4a9b0eea48380.bundle.js"
+  },
+  {
+    "url": "runtime.e9e51b0120293c3f6627.bundle.js"
+  },
+  {
+    "url": "target-blank.4.7d0a661b3a49da6bab77.bundle.js"
+  },
+  {
+    "url": "vendor.5.f5c51af1a4c7ac47627b.bundle.js"
+  },
+  {
+    "url": "vendor.5.f5c51af1a4c7ac47627b.css"
+  },
+  {
+    "url": "vendors~Sidebar.0.f1a43da1c17e398b1a20.bundle.js"
+  },
+  {
+    "url": "vendors~Sidebar.0.f1a43da1c17e398b1a20.css"
+  },
+  {
+    "url": "vendors~live.6.d95993cb882e73806103.bundle.js"
+  },
+  {
+    "url": "vendors~live.6.d95993cb882e73806103.css"
+  }
+]);
